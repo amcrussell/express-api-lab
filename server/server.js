e817a2b10d8bcdaa203dec4f000958ee8573a630
Austin <amcrussell03@gmail.com>
2023-12-31 14:28:56 -0500

    removed unnecesary code

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const apiRouter = require('./routes');
 const path = require('path');
-const bodyParser = require('body-parser');
 
 let app = express();
 
